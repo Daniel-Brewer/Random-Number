a9932cf8ce356deeef9297d4a300115bcbf6f24b
Daniel Brewer <danielbrewer2014@comcast.net>
2019-03-04 09:53:25 -0600

    completed exercise

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace random_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+List<int> numbers = new List<int> {
+    random.Next(10),
+    random.Next(10),
+    random.Next(10),
+    random.Next(10),
+    random.Next(10),
+};
+for (int i = 0; i < numbers.Count - 1; i++)
+{
+    if (numbers[i] == i)
+    {
+        Console.WriteLine($"Numbers list contains {i}");
+    }
+    else
+    {
+       Console.WriteLine($"numbers list does not contain {i}");
+    }
+    
+}
+        }
+    }
+}
